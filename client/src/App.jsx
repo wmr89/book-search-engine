0c4a322ab606c162173c79f6ab47ec20056d66ba
williamrankin <william.rankin01@gmail.com>
2024-03-26 21:24:06 -0600

    added apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
