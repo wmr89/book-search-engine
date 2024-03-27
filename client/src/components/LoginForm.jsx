266e35e3c32276024fa580cd14219e10cf14e2d6
williamrankin <william.rankin01@gmail.com>
2024-03-26 21:24:17 -0600

    imported from mutations

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -5,6 +5,8 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
+import { LOGIN_USER } from '../utils/mutations';
+
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
