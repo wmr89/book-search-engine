04a15598c8cbb5a68877173365a0b260dd12e6d6
williamrankin <william.rankin01@gmail.com>
2024-03-26 21:25:20 -0600

    added file, framework for mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation loginUser(){
+    loginUser() {
+        x
+        x
+        x
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser(){
+    addUser() {
+        x
+        x
+        x
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook(){
+    saveBook() {
+        x
+        x
+        x
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook(){
+    removeBook() {
+        x
+        x
+        x
+    }
+}
+`;
